92e75df73da7a51d66255dad193e7069268e1fb4
zvonedev04 <zvonimirturkalj04@gmail.com>
2026-05-11 13:53:06 +0200

    brojac novcanica

---
+++ script.js
@@ -0,0 +1,13 @@
+let novcanice = [10, 5, 20, 50, 5, 10, 100, 20, 50];
+let broj_novcanica = novcanice.length;
+let brojac = 0;
+
+while (brojac < broj_novcanica) {
+    console.log('------------------');
+    console.log('uslov prosao');
+    console.log(`novcanica na poziciji niza [${brojac}]: ${novcanice[brojac]}`)
+    
+    brojac++;
+}
+
+
